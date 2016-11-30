8dfff766e6ea2071d0bc73d79b4833acbfad0358
magik4rp <notfelineit@berkeley.edu>
2016-11-29 18:13:38 -0800

    Frances pushing on heroku

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-// var express = require('express')
-// var app = express()
-// app.use("/", express.static(__dirname + '/public'));
-// app.get('/', function (req, res) {
-//   res.send('home.html')
-// })
-
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
-
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/home');
+  response.render('home/index');
 });
 
 app.listen(app.get('port'), function() {
