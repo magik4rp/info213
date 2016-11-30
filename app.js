c2d4632ad24138db6f03c32514376ed562c48b39
magik4rp <notfelineit@berkeley.edu>
2016-11-29 18:07:01 -0800

    Frances: reorganized

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-var express = require('express')
-var app = express()
-app.use("/", express.static(__dirname + '/public'));
-app.get('/', function (req, res) {
-  res.send('home.html')
-})
+// var express = require('express')
+// var app = express()
+// app.use("/", express.static(__dirname + '/public'));
+// app.get('/', function (req, res) {
+//   res.send('home.html')
+// })
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
 
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/home');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
