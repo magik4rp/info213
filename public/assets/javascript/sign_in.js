593fdf4eb65f0df597393b66b554983a1e8c118b
magik4rp <notfelineit@berkeley.edu>
2016-12-01 19:49:21 -0800

    Added show/hide password functionality for error prevention

---
+++ public/assets/javascript/sign_in.js
@@ -0,0 +1,23 @@
+$("#sign-in-container").ready(function() 
+    { $("#show-password").click(function() {
+      console.log("CLICKED");
+      var $input = $("#password");
+      var change = "";
+      if ($(this).html() == "Show Password") {
+        $(this).html("Hide Password")
+        change = "text";
+      } else {
+        $(this).html("Show Password");
+        change = "password";
+      }
+          var rep = $("<input type='" + change + "' />")
+            .attr("id", $input.attr("id"))
+            .attr("name", $input.attr("name"))
+            .attr('class', $input.attr('class'))
+            .attr('placeholder', $input.attr('placeholder'))
+            .val($input.val())
+            .insertBefore($input);
+          $input.remove();
+          $input = rep;
+      $(this).insertAfter($input);
+    });})
